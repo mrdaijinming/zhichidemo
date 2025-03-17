005eb851a068d3cbdec20ebbccab0507abbc2a1b
这玩玩吃包子不给钱 <15712897291@163.com>
2025-03-17 16:11:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express =require('express')
+const express =require('express');
+const http=require('http');
 const app = express();
+const server = http.createServer((req,res)=>{
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('Hello World!');
+});
 app.use(express.static('public'));
 const port =80;
 app.listen(port,()=>{
-  console.log('Server running at http://localhost:$(port)/');
+  console.log(`Server running at http://localhost:$(port)/`);
 })
